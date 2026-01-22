5f3dcb603e0fa42c278301d011d9ada72121e6d7
Muhammad Dafha Syahrizal <143611789+zalb0y@users.noreply.github.com>
2026-01-22 09:10:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def create_line_chart_plotly(df_filtered, filter_stores, all_stores, filter_mode
         if 'dormant' in keywords_lower:
             display_keywords.append('DORMANT')
         if 'new regis' in keywords_lower or 'nr' in keywords_lower:  # Tambah 'nr'
-            display_keywords.append('NR')
+            display_keywords.append('New Regis')
         coupon_text = ", ".join(display_keywords) if display_keywords else "Custom Keywords"
     else:
         if len(selected_coupons) <= 3:
