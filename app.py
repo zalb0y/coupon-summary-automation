4c3eed7c3576ea599c41633e55c06958c968feb4
Muhammad Dafha Syahrizal <143611789+zalb0y@users.noreply.github.com>
2026-02-06 14:09:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -701,7 +701,7 @@ def main():
         )
         
         if filter_mode == 'Keywords':
-            default_keywords = ['tm', 'new regis', 'dormant']
+            default_keywords = ['tm', 'ka', '1kg', 'dormant']
             coupon_keywords_input = st.text_input(
                 "Enter keywords (comma-separated)",
                 value=', '.join(default_keywords)
